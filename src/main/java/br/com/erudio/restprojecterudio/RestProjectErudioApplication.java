0e8a1d6acf496f42bd564b542307f15348a193f0
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-03-22 06:15:59 -0300

    initial commit

---
+++ src/main/java/br/com/erudio/restprojecterudio/RestProjectErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restprojecterudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProjectErudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestProjectErudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/erudio/restprojecterudio/RestProjectErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restprojecterudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectErudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
