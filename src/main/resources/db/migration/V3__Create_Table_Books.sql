7bf3c673dd0cea86b930788fe465fea953888edf
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-07-24 22:19:52 -0300

    implementation migrations using flyway to create permissions and user table

---
+++ src/main/resources/db/migration/V3__Create_Table_Books.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE books
+(
+    id serial PRIMARY KEY,
+    author varchar ,
+    launch_date date NOT NULL,
+    price decimal(65,2) NOT NULL,
+    title varchar
+);
---
+++ src/main/resources/db/migration/V5__Create_Table_Permission.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE IF NOT EXISTS permission
+(
+    id          SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
