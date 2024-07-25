8179dde1d97085137a5ea11d7ce8ece04cb8ba76
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-07-24 22:20:01 -0300

    implementation migrations using flyway to create permissions and user table

--- src/main/resources/db/migration/V1__Create_Table_Person.sql
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -2,9 +2,9 @@
 CREATE TABLE IF NOT EXISTS public.person
 (
     id serial NOT NULL ,
-    first_name character(60) NOT NULL,
-    last_name character(50) NOT NULL,
-    address character(60) NOT NULL,
-    gender character(60) NOT NULL,
+    first_name character(10) NOT NULL,
+    last_name character(10) NOT NULL,
+    address character(20) NOT NULL,
+    gender character(6) NOT NULL,
     PRIMARY KEY (id)
 );
